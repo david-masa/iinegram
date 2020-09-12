c56b139ace0a35bcd6157e8e13dfada21b007722
david-masa <60571920+david-masa@users.noreply.github.com>
2020-09-12 15:25:51 +0900

    投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,13 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get '/users/:id', to: 'users#show', as: 'user'
+
+  resources :posts, only: %i(new create) do
+    resources :photos, only: %i(create)
+  end
+  # get'/posts/new',to: 'posts#new'
+  # get'/posts', to:'posts#create'
+  # post'/posts/:post_id/photos',to:'photos#create',as: 'post_photos'
+  # 上のコードの別の書き方
+
 end
